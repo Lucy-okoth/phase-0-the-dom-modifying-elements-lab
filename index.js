682b9bc425bedac23bfaf64b03a6544d2e2eb1e7
Lucy okoth <lucy.okoth@student.moringaschool.com>
2022-08-21 20:52:00 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+// Write your code here!const ul = document.createElement("ul");
+
+var deleteMain = document.getElementById('main').remove()
+
+
+const newHeader = document.createElement('h1')
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Lucy is the champion';
+
+newHeader.append()
